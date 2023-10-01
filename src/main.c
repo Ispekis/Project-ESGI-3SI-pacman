e2514f43d0dbe12e4abed5a4b5eef6ae6046b929
Vincent Shao <vshao2@myges.fr>
2023-10-01 19:44:26 +0200

    feat: update gitignore for default

--- src/main.c
+++ src/main.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 
-int main(int ac __attribute__((unused)), char **av __attribute__((unused)))
-{
+int main(int ac __attribute__((unused)), char **av __attribute__((unused))) {
     printf("Hello world\n");
     return 0;
 }
